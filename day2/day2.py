30d92929a7cd2234aad5f9809d1852fbee94bfee
David Barnes <dbarnes@kvcc.edu>
2024-12-07 18:05:24 -0500

    Finish day 2.

--- day2/day2.py
+++ day2/day2.py
@@ -1,5 +1,7 @@
 """Day 2 module"""
 
+from copy import deepcopy
+
 
 class Day2:
     """Class for solving day 2 problem"""
@@ -25,9 +27,78 @@ class Day2:
     def solve_problem_1(self):
         """Solve problem 1"""
 
-        self.ui.print_warning("Solution not implemented yet.")
+        total = 0
+        # Get the lines of input
+        lines = self.file_reader.read_lines("day2/input.txt")
+
+        for line in lines:
+            original = line.split()
+            original = [int(element) for element in original]
+
+            unique = deepcopy(original)
+            unique = len(set(unique)) == len(original)
+
+            ascending = deepcopy(original)
+            ascending.sort()
+            is_ascending = ascending == original
+
+            descending = deepcopy(original)
+            descending.sort(reverse=True)
+            is_descending = descending == original
+
+            if unique and (is_ascending or is_descending):
+
+                if self.determine_if_list_is_valid(original):
+                    total += 1
+
+        # Print success of value.
+        self.ui.print_success(f"The total is: {total}")
 
     def solve_problem_2(self):
         """Solve problem 2"""
 
-        self.ui.print_warning("Solution not implemented yet.")
+        total = 0
+        # Get the lines of input
+        lines = self.file_reader.read_lines("day2/input.txt")
+
+        for line in lines:
+            original = line.split()
+            original = [int(element) for element in original]
+
+            altered_is_valid = False
+
+            for i in range(len(original)):
+
+                altered = deepcopy(original)
+                altered.pop(i)
+
+                unique = deepcopy(altered)
+                unique = len(set(unique)) == len(altered)
+
+                ascending = deepcopy(altered)
+                ascending.sort()
+                is_ascending = ascending == altered
+
+                descending = deepcopy(altered)
+                descending.sort(reverse=True)
+                is_descending = descending == altered
+
+                if unique and (is_ascending or is_descending):
+
+                    if self.determine_if_list_is_valid(altered):
+                        altered_is_valid = True
+
+            if altered_is_valid:
+                total += 1
+
+        # Print success of value.
+        self.ui.print_success(f"The total is: {total}")
+
+    def determine_if_list_is_valid(self, the_list):
+        """Determine if the list is valid"""
+        line_valid = True
+        for i in range(len(the_list) - 1):
+            diff = abs(the_list[i] - the_list[i + 1])
+            if diff < 1 or diff > 3:
+                line_valid = False
+        return line_valid
